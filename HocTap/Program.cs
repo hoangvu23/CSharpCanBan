3d303fdcc2a752eabc43c30ede1098fa2666819b
hoang vu <vumonglep@gmail.com>
2023-10-11 21:07:39 +0700

    init project

---
+++ HocTap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HocTap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chào Vũ !");
+        }
+    }
+}
